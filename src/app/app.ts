8d541b5e6e80e69b6fc14d112e388575d3cb26c7
EMARESA\fromero <fromero@emaresa.cl>
2025-07-17 10:25:50 -0400

    feature : request mock students by get req

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toolbar } from './toolbar/toolbar';
 import { Navbar } from './navbar/navbar';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../shared/entities';
+import { CommonModule } from '@angular/common';
+import { StudentsTable } from './students-table/students-table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toolbar, Navbar],
+  imports: [RouterOutlet, Toolbar, Navbar, CommonModule, StudentsTable],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'Fromero-E01';
+export class App implements OnInit {
+  students: Student[] = [];
+  constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Initialization logic can go here
+      this.http.get<Student[]>('mocks/students.json').subscribe(data => {
+        this.students = data;
+    });
+  }
+
 }
