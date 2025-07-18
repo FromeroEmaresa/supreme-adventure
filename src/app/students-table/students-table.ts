8435a31c569f45e820d99a060f527bb5179288f3
EMARESA\fromero <fromero@emaresa.cl>
2025-07-18 10:10:00 -0400

    feature: añadir diseño bootstrap a students

--- src/app/students-table/students-table.ts
+++ src/app/students-table/students-table.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Student } from '../../shared/entities';
 
 import { MatTableModule } from '@angular/material/table';
@@ -13,5 +13,20 @@ import { FullnamePipe } from '../../shared/pipes/fullname-pipe';
 export class StudentsTable {
   @Input() students: Student[] = [];
 
-  displayedColumns: string[] = ['fullname', 'age', 'dni', 'average'];
+  displayedColumns: string[] = ['fullname', 'age', 'dni', 'average', 'actions'];  
+
+  @Output() eliminar: EventEmitter<Student> = new EventEmitter<Student>();
+
+  eliminarEstudiante(estudiante: Student) {
+    this.eliminar.emit(estudiante);
+  }
+
+  @Output() editar = new EventEmitter<Student>();
+
+  editarEstudiante(estudiante: Student) {
+    this.editar.emit(estudiante);
+  }
+
+
+
 }
